7230eb50472bc47a0810dcdcce5b22e8b1bab5c0
Vatsal3255 <58387837+Vatsal3255@users.noreply.github.com>
2023-03-07 13:37:23 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,23 +3,23 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = "6059453194:AAHvwYfK3sen6fok5DKe83M6jz1C7SCzcrg"
 
     SESSION_NAME = ":memory:"
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = 2943728
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = "f92341ae32e29aa1bfdf3b5a5d25a80a"
 
-    CLIENT_ID = os.environ.get("CLIENT_ID")
+    CLIENT_ID = "1005570709419-6k5es908759kn9up85pm43sh47ft4tpv.apps.googleusercontent.com"
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_SECRET = "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"
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = 535883227
 
-    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS_TEXT = "535883227"
 
-    AUTH_USERS = [BOT_OWNER, 754495556] + (
+    AUTH_USERS = [BOT_OWNER] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
         else []
